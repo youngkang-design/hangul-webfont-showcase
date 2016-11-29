87e154e4976fc3f6c72db7d4acb351b514d21f99
Seungha Kim <seungha.me@gmail.com>
2016-11-30 04:18:44 +0900

    optimization for ios

--- src/sagas/fontLoader.js
+++ src/sagas/fontLoader.js
@@ -19,13 +19,13 @@ export default function* fontLoader(document) {
 function* loadFont(document, {payload: {groupIndex, itemIndex}}) {
   const {url, family} = getFontItem(data, groupIndex, itemIndex)
   const observer = new FontFaceObserver(family)
-  yield put(createAction(CurtainEntering))
   if (url && !isFontLoaded(document, url)) {
     const link = document.createElement('link')
     link.rel = 'stylesheet'
     link.href = url
     document.head.appendChild(link)
   }
+  yield put(createAction(CurtainEntering))
   yield delay(500)
   const indicatorTask = yield fork(showLoadingIndicator)
   yield observer.load()
