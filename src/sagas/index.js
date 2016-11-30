77962549d7127cfbc1193d2be8b06f77da6b28fb
Seungha Kim <seungha.me@gmail.com>
2016-12-01 07:13:37 +0900

    remove console.log

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -4,7 +4,6 @@ import home from './home'
 import mobile from './mobile'
 
 export default function* rootSaga(window) {
-  console.log('Hello Sagas!')
   yield [
     fontLoader(window, document),
     historySaga(history),
