f0b7dd2b82aec9359ccb81ea55a916b8850ec041
young-hwa <youngkang.design@gmail.com>
2016-12-03 03:25:15 +0900

    change black option of young-hwa's website link.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ export default class Footer extends React.Component {
   render() {
       return <div className={s.wrap}>
         <div className={s.responsive}>
-          <a href="http://youngkang.me">강영화</a> 디자인∙<a href="mailto:seungha.me@gmail.com">김승하</a> 도움 <br/>
+          <a href="http://youngkang.me" target="_blank">강영화</a> 디자인∙<a href="mailto:seungha.me@gmail.com">김승하</a> 도움 <br/>
           이 웹페이지의 저작권은 <a href="http://youngkang.me">강영화</a>에게 있습니다.
         </div>
       </div>
